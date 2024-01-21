9b71f76262c998df3b301c2a28ed78078d7d86d3
javaScript.js <demidkoanastacia@yandex.bym>
2024-01-21 18:42:57 +0300

    правки  по 2 занятию

--- javaScript.js
+++ javaScript.js
@@ -9,8 +9,9 @@ console.log(typeof money);
 console.log(typeof income);
 console.log(typeof deposit);
 console.log(addExpenses.length);
-console.log('Период равен ${period} месяцев и цель заработать ${mission} рублей/долларов/гривен/юани');
-console.log(addExpenses.split(addExpenses.toLocaleLowerCase()));
+console.log(`Период равен ${period} месяцев и цель заработать ${mission} рублей/долларов/гривен/юани`);
+addExpenses.toLocaleLowerCase()
+console.log(addExpenses.split(', '));
 
 let budgetDay = money / 30;
 console.log(budgetDay);
