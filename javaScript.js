d6511851200b94f46064e8138b92f50315b49e65
javaScript.js <demidkoanastacia@yandex.bym>
2024-01-22 22:14:07 +0300

    lesso04

--- javaScript.js
+++ javaScript.js
@@ -6,22 +6,39 @@ const deposit = confirm('Есть ли у вас депозит в банке?')
 const mission = 15000;
 const period = 5
 
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
+const showTypeOf = function(date){
+    console.log(date, typeof(date));
+}
+showTypeOf(money);
+showTypeOf(income);
+showTypeOf(deposit);
+
 console.log(addExpenses.length);
 console.log(`Период равен ${period} месяцев и цель заработать ${mission} рублей/долларов/гривен/юани`);
 addExpenses.toLocaleLowerCase()
 console.log(addExpenses.split(', '));
 
 const expenses1 = prompt('Введите обязательную статью расходов');
-const amount1 = prompt('Во сколько это обойдется?');
+const amount1 = +prompt('Во сколько это обойдется?');
 
-const expenses2 = +prompt('Введите обязательную статью расходов');
+const expenses2 = prompt('Введите обязательную статью расходов');
 const amount2 = +prompt('Во сколько это обойдется?');
 
-const budgetMonth = money - amount1 + amount2;
-const months = Math.ceil(mission / budgetMonth);
+function getExpensesMonth (sum1, sum2) {
+    return sum1 + sum2;    
+}
+
+const amount = getExpensesMonth(amount1, amount2);
+
+function getAccumulatedMonth (income, expenses) {
+   return income - expenses; 
+}
+
+const accumulatedMonth = accumulatedMonth(money, amount);
+
+
+
+const months = Math.ceil(mission / accumulatedMonth);
 console.log(months);
 
 const budgetDay = Math.floor(budgetMonth / 30);
--- javaScript_.js
+++ javaScript_.js
@@ -30,3 +30,17 @@ let namePerson = "Артем";
 var role = namePerson === "Артем" ? "Директор" : namePerson === "Максим" ? "Преподаватель" : "Студент";
 
 console.log(role);
+
+const str =  3;
+
+const func = function (string) {
+
+  if (string === 'string') {
+    let newStr = string.trim();
+    return (newStr.length <= 30) ? newStr : newStr.substring(0, 30) + '...';
+  } else {
+    return 'Передан неправильный тип данных';
+  }
+}
+
+console.log(func(str));
